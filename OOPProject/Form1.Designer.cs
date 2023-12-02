c4025988c5d2d2196f6a15d423b0efd7519c6795
Najm-Ur-Rehman <67186639+Najaam@users.noreply.github.com>
2023-12-02 21:14:44 +0500

    Add project files.

---
+++ OOPProject/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace OOPProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.Rolebox = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(49, 332);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 35);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(49, 278);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 35);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(45, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(134, 35);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Username";
+            // 
+            // txtname
+            // 
+            this.txtname.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtname.Location = new System.Drawing.Point(234, 172);
+            this.txtname.Multiline = true;
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(386, 42);
+            this.txtname.TabIndex = 5;
+            // 
+            // txtusername
+            // 
+            this.txtusername.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtusername.Location = new System.Drawing.Point(234, 229);
+            this.txtusername.Multiline = true;
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(386, 42);
+            this.txtusername.TabIndex = 6;
+            // 
+            // txtemail
+            // 
+            this.txtemail.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtemail.Location = new System.Drawing.Point(234, 278);
+            this.txtemail.Multiline = true;
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(386, 42);
+            this.txtemail.TabIndex = 7;
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpassword.Location = new System.Drawing.Point(234, 333);
+            this.txtpassword.Multiline = true;
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(386, 42);
+            this.txtpassword.TabIndex = 8;
+            // 
+            // Rolebox
+            // 
+            this.Rolebox.AutoCompleteCustomSource.AddRange(new string[] {
+            "Seller",
+            "Buyer"});
+            this.Rolebox.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Rolebox.FormattingEnabled = true;
+            this.Rolebox.Items.AddRange(new object[] {
+            "Buyer",
+            "Seller"});
+            this.Rolebox.Location = new System.Drawing.Point(479, 392);
+            this.Rolebox.Name = "Rolebox";
+            this.Rolebox.Size = new System.Drawing.Size(141, 37);
+            this.Rolebox.TabIndex = 9;
+            this.Rolebox.Text = "Select Role";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(306, 424);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 48);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Register";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(271, 486);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(226, 24);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Already Have a Account??";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(315, 513);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 49);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Login";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 634);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Rolebox);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.txtusername);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.ComboBox Rolebox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ OOPProject/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace OOPProject
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn = new SqlConnection("Data Source=DESKTOP-5EIA8UT\\SQLEXPRESS;Initial Catalog=OOP;Integrated Security=True");
+
+        class User
+        {
+            public string Name { get; set; }
+            public string Username { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+            public int Otp { get; set; }
+
+            public User(string name, string username, string email, string password, int otp)
+            {
+                Name = name;
+                Username = username;
+                Email = email;
+                Password = password;
+                Otp = otp;
+            }
+
+            public string saveData(string role)
+            {
+                return $"INSERT INTO {role.ToLower()} VALUES ('{Name}', '{Username}', '{Email}', '{Password}', '{Otp}')";
+            }
+        }
+
+        class UserRegistrationManager
+        {
+            private SqlConnection Connection { get; }
+
+            public UserRegistrationManager(SqlConnection connection)
+            {
+                Connection = connection;
+            }
+
+            public void RegisterUser(User user, string role)
+            {
+                SqlCommand cmd = new SqlCommand(user.saveData(role), Connection);
+                try
+                {
+                    Connection.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show($"You are successfully registered as {role}. Please login to your account.", "Message");
+                }
+                catch (SqlException ex)
+                {
+                    if (ex.Number == 2627)
+                    {
+                        MessageBox.Show("Username is already taken. Please choose a different username.", "Error");
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Error: {ex.Message}", "Error");
+                    }
+                }
+                Connection.Close();
+            }
+
+            public int GenerateOTP()
+            {
+                Random rand = new Random();
+                return rand.Next(1000, 9999);
+            }
+
+            public void SendOtpByEmail(string toEmail, int otp)
+            {
+
+                string smtpServer = "smtp.gmail.com";
+                string smtpUsername = "najamurrehman912@gmail.com";
+                string smtpPassword = "sygg uauu kiec rehf";
+                int smtpPort = 587;
+
+                SmtpClient smtpClient = new SmtpClient(smtpServer)
+                {
+                    Port = smtpPort,
+                    Credentials = new NetworkCredential(smtpUsername, smtpPassword),
+                    EnableSsl = true,
+                };
+
+                MailMessage mailMessage = new MailMessage
+                {
+                    From = new MailAddress("najamurrehman912@gmail.com", "Ecom"),
+                    Subject = "OTP Verification",
+                    Body = $"Your OTP is: {otp}",
+                    IsBodyHtml = false,
+                };
+
+                mailMessage.To.Add(toEmail);
+
+                try
+                {
+                    smtpClient.Send(mailMessage);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Failed to send email. Error: {ex.Message}", "Error");
+                }
+            }
+
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UserRegistrationManager registrationManager = new UserRegistrationManager(conn);
+            string name = txtname.Text;
+            string username = txtusername.Text;
+            string email = txtemail.Text;
+            string password = txtpassword.Text;
+            int otp = registrationManager.GenerateOTP();
+
+            string role = Rolebox.Text.ToLower();
+
+            User newUser = new User(name, username, email, password,otp);
+
+            registrationManager.RegisterUser(newUser, role);
+            registrationManager.SendOtpByEmail(email, otp);
+           
+        }
+    }
+}
